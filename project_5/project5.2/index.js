4b49c268c2b1082f1fff8c509c0f7e72063c3f91
kaiyang <KKYKHAW03.23@ichat.sp.edu.sg>
2024-10-20 14:41:49 +0800

    learning java scribt now

---
+++ project_5/project5.2/index.js
@@ -0,0 +1,14 @@
+let x;
+x=100;
+console.log(x);
+
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+gpa= price+gpa;
+let age_price=age*price;
+
+console.log(`your gpa is ${gpa}.`);
+console.log(`The price of your bannana is ${price}`);
+console.log(`the age of your cousin is ${age}.`);
+console.log(`this doesn't make send but the mutiple of gpa and price is ${age_price}.`);
